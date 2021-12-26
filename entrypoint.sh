8a3f9a169624a4243a9817a727eeaff8d95b5d14
ridgarou <ridgarou@gmail.com>
2021-12-26 19:23:23 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,6 +20,11 @@ echo ""
 echo "# Adding cronjob and starting..."
 echo '0,15,30,45 * * * * /app/Auto_Books_Calibre.sh >> /Books_Calibre_Backup/01-Calibre.log 2>&1' | crontab
 service cron start
+
+if [ ! -f /app/edited ]; then
+    sed -i 's/type="email" class="form-control" name="kindle_mail"/type="text" class="form-control" name="kindle_mail"/g' /app/calibre-web/cps/templates/user_edit.html
+    touch /app/edited
+fi
 echo ""
 echo "###########################"
 echo "# Config completed        #"
